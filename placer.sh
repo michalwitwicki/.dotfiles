cc82a4aa7d558e6c48bd5d1d5075b6bec4bad13e
Batawi <michal.witwicki14@gmail.com>
2020-01-09 22:07:47 +0100

    placer.sh: init

---
+++ placer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This script will make links to config files from appropriate places
+
+#Destination paths
+bashrch_path="~/"
+tmux_conf_path="~/"
+alacritty_conf_path="~/.config/alacritty"
+nvim_conf_path="~/.config/nvim"
+
+#file="~/link_test/to_remove"
+#eval rm -f $file
+
+file=~/link_test/to_remove
+rm -f "$file"
