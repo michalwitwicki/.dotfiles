23e22ddc8896b6719f991a3b0d16434ab0cb5cb5
Michal Witwicki <michal.witwicki@intel.com>
2023-06-15 14:52:15 +0200

    Add nvim-tree open on start
    Signed-off-by: Michal Witwicki <michal.witwicki@intel.com>

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -49,7 +49,29 @@ require("nvim-tree").setup({
         open_file = {
             resize_window = false
         }
+    },
+    update_focused_file = {
+        enable = true
     }
 })
 
-vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>e", ":lua require('nvim-tree.api').tree.toggle({find_file = true})<CR>")
+
+local function open_nvim_tree(data)
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
