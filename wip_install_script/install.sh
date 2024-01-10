a1fe56a763c6343f3c194c13b27418f977665dc1
Michal Witwicki <michal.witwicki@intel.com>
2024-01-10 17:43:03 +0100

    WIP install script
    Signed-off-by: Michal Witwicki <michal.witwicki@intel.com>

--- wip_install_script/install.sh
+++ wip_install_script/install.sh
@@ -2,7 +2,7 @@
 
 mkdir tools
 wget -P ./tools https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-wget -P ./tools https://github.com/dylanaraps/fff/archive/refs/tags/2.2.tar.gz
+wget -O ./tools/fff.tar.gz https://github.com/dylanaraps/fff/archive/refs/tags/2.2.tar.gz
 wget -P ./tools https://github.com/andreafrancia/trash-cli/archive/refs/tags/0.23.11.10.tar.gz
 wget -P ./tools https://github.com/dandavison/delta/releases/download/0.16.5/delta-0.16.5-x86_64-unknown-linux-gnu.tar.gz
 wget -P ./tools https://github.com/junegunn/fzf/releases/download/0.44.1/fzf-0.44.1-linux_amd64.tar.gz
