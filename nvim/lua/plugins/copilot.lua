618722968927b26d22c484e2bee9c37455aa82c1
Michal Witwicki <michal.witwicki14@gmail.com>
2024-04-05 10:05:10 -0400

    Add Github Copilot plugins
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,103 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            })
+        end,
+    },
+    {
+        'zbirenbaum/copilot-cmp',
+        dependencies = {
+            {'zbirenbaum/copilot.lua'},
+        },
+        config = function ()
+            require("copilot_cmp").setup()
+        end
+        -- copilot-cmp is added as a source in cmp.setup, in lsp.lua file
+    },
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        -- branch = 'canary',
+        version = "v2.4.2",
+        dependencies = {
+            {'zbirenbaum/copilot.lua'},
+            {'nvim-telescope/telescope.nvim'}, -- Use telescope for help actions
+            {'nvim-lua/plenary.nvim'}, -- for curl, log wrapper
+        },
+        opts = {
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        event = "VeryLazy",
+        keys = {
+            -- Show help actions with telescope
+            {
+                "<leader>ah",
+                function()
+                    local actions = require("CopilotChat.actions")
+                    require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+                end,
+                desc = "CopilotChat - Help actions",
+            },
+            -- Show prompts actions with telescope
+            {
+                "<leader>ap",
+                function()
+                    local actions = require("CopilotChat.actions")
+                    require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+                end,
+                desc = "CopilotChat - Prompt actions",
+            },
+            {
+                "<leader>ap",
+                ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
+                mode = "x",
+                desc = "CopilotChat - Prompt actions",
+            },
+            {
+                "<leader>av",
+                function()
+                    local input = vim.fn.input("Chat about visual selection: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
+                    end
+                end,
+                mode = "x",
+                desc = "CopilotChat - Select visual selection and ask",
+            },
+            -- Custom input for CopilotChat
+            {
+                "<leader>ai",
+                function()
+                    local input = vim.fn.input("Ask Copilot: ")
+                    if input ~= "" then
+                        vim.cmd("CopilotChat " .. input)
+                    end
+                end,
+                desc = "CopilotChat - Ask input",
+            },
+            -- Quick chat with Copilot
+            {
+                "<leader>ab",
+                function()
+                    local input = vim.fn.input("Chat about whole buffer: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+                    end
+                end,
+                desc = "CopilotChat - Select whole buffer and ask",
+            },
+            { "<leader>ad", "<cmd>CopilotChatDebugInfo<cr>", desc = "CopilotChat - Debug Info" },
+            { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
+            { "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
+            { "<leader>av", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+            { "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+            { "<leader>at", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+        },
+    }
+}
