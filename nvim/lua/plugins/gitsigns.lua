ec4a9adf03d6b8860075efa7cd952e8a733ff28b
Michal Witwicki <michal.witwicki14@gmail.com>
2024-09-20 04:06:13 -0400

    update plugins, add blame keymaps
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,6 +10,15 @@ return {
                 changedelete = { text = '~' },
                 untracked    = { text = '┆' },
             },
+            signs_staged = {
+                add          = { text = '│' },
+                change       = { text = '│' },
+                delete       = { text = '_' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
+            },
+            signs_staged_enable = true,
             signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
             numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
             linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -18,7 +27,7 @@ return {
                 follow_files = true
             },
             auto_attach = true,
-            attach_to_untracked = true,
+            attach_to_untracked = false,
             current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = {
                 virt_text = true,
@@ -26,8 +35,9 @@ return {
                 delay = 1000,
                 ignore_whitespace = false,
                 virt_text_priority = 100,
+                use_focus = true,
             },
-            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
@@ -40,11 +50,8 @@ return {
                 row = 0,
                 col = 1
             },
-            yadm = {
-                enable = false
-            },
             on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
+                local gitsigns = require('gitsigns')
 
                 local function map(mode, l, r, opts)
                     opts = opts or {}
@@ -54,16 +61,24 @@ return {
 
                 -- Navigation
                 map('n', ']c', function()
-                    if vim.wo.diff then return ']c' end
-                    vim.schedule(function() gs.next_hunk() end)
-                    return '<Ignore>'
-                end, {expr=true})
+                    if vim.wo.diff then
+                        vim.cmd.normal({']c', bang = true})
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
 
                 map('n', '[c', function()
-                    if vim.wo.diff then return '[c' end
-                    vim.schedule(function() gs.prev_hunk() end)
-                    return '<Ignore>'
-                end, {expr=true})
+                    if vim.wo.diff then
+                        vim.cmd.normal({'[c', bang = true})
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end)
+
+                -- Actions
+                map('n', '<leader>gl', function() gitsigns.blame_line{full=false} end)
+                map('n', '<leader>gb', gitsigns.blame)
 
             end
         }
