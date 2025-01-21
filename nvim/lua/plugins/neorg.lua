f6b9d4f941be3be1d9700e7423f47e3195affee9
Michal Witwicki <michal.witwicki14@gmail.com>
2025-01-21 07:03:26 -0500

    add neorg default keybinds
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -28,3 +28,39 @@ return {
         vim.keymap.set('n', '<leader>nc', ':Neorg toggle-concealer<CR>')
     end,
 }
+
+-- Default keybindings
+
+-- All files
+-- <LocalLeader>nn - create a new .norg file to take notes in
+
+-- Norg only, insert mode
+-- <C-d> - demote an object recursively
+-- <C-t> - promote an object recursively
+-- <M-CR> - create an iteration of e.g. a list item
+-- <M-d> - insert a link to a date at the current cursor position
+
+-- Norg only, normal mode
+-- <, - demote an object non-recursively
+-- << - demote an object recursively
+-- <C-Space> - switch the task under the cursor between a select few states
+-- <CR> - hop to the destination of the link under the cursor
+-- <LocalLeader>cm - magnifies a code block to a separate buffer.
+-- <LocalLeader>id - insert a link to a date at the given position
+-- <LocalLeader>li - invert all items in a list
+-- <LocalLeader>lt - toggle a list from ordered <-> unordered
+-- <LocalLeader>ta - mark the task under the cursor as "ambiguous"
+-- <LocalLeader>tc - mark the task under the cursor as "cancelled"
+-- <LocalLeader>td - mark the task under the cursor as "done"
+-- <LocalLeader>th - mark the task under the cursor as "on-hold"
+-- <LocalLeader>ti - mark the task under the cursor as "important"
+-- <LocalLeader>tp - mark the task under the cursor as "pending"
+-- <LocalLeader>tr - mark the task under the cursor as "recurring"
+-- <LocalLeader>tu - mark the task under the cursor as "undone"
+-- <M-CR> - same as <CR>, except open the destination in a vertical split
+-- >. - promote an object non-recursively
+-- >> - promote an object recursively
+
+-- Norg only, visual mode
+-- < - demote objects in range
+-- > - promote objects in range
