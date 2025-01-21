4d68b844cbfce641bf53d487d55cb336cbd20188
Michal Witwicki <michal.witwicki14@gmail.com>
2025-01-21 06:29:56 -0500

    add neorg binds
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -20,5 +20,10 @@ return {
 
         vim.wo.foldlevel = 99
         vim.wo.conceallevel = 2
+
+        vim.keymap.set('n', '<leader>nn', '<Plug>(neorg.dirman.new-note)')
+        vim.keymap.set('n', '<leader>ns', ':vnew<CR>:Neorg index<CR>')
+        vim.keymap.set('n', '<leader>nt', ':tabnew<CR>:Neorg index<CR>')
+        vim.keymap.set('n', '<leader>nr', ':Neorg return<CR>')
     end,
 }
