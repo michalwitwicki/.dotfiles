71598ba990e61b4a52a9a03b24d654394910ca19
Michal Witwicki <michal.witwicki14@gmail.com>
2025-11-08 12:01:01 +0100

    fix depracated leap function
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,8 +1,8 @@
 return {
     'ggandor/leap.nvim',
     config = function()
-        vim.keymap.set({'n', 'x', 'o'}, '<leader>l', '<Plug>(leap-forward-to)')
-        vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '<Plug>(leap-backward-to)')
+        vim.keymap.set({'n', 'x', 'o'}, '<leader>l', '<Plug>(leap-forward)')
+        vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '<Plug>(leap-backward)')
     end
 }
 
