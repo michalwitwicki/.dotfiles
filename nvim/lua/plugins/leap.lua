1feca849b6c2d69d1b4664ae9c89a3fb6da3247a
Michal Witwicki <michal.witwicki14@gmail.com>
2025-11-27 16:00:38 +0100

    minor changes to various plugins
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,8 +1,11 @@
 return {
-    'ggandor/leap.nvim',
-    config = function()
-        vim.keymap.set({'n', 'x', 'o'}, '<leader>l', '<Plug>(leap-forward)')
-        vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '<Plug>(leap-backward)')
-    end
+	'ggandor/leap.nvim',
+	config = function()
+		local leap = require('leap')
+		leap.opts.vim_opts['go.ignorecase'] = true
+
+		vim.keymap.set({'n', 'x', 'o'}, '<leader>l', '<Plug>(leap-forward)')
+		vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '<Plug>(leap-backward)')
+	end
 }
 
