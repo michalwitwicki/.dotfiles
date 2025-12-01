8f690848b1d1607937bd4838a8b344d2647a625c
Michal Witwicki <michal.witwicki14@gmail.com>
2025-12-01 11:26:50 +0100

    config fzf-lua plugin
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -11,30 +11,55 @@ return {
     { "<leader>fk",  function() require("fzf-lua").keymaps() end,         desc = "search keymaps" },
     { "<leader>fs",  function() require("fzf-lua").live_grep() end,       desc = "live grep" },
     { "<leader>fs",  function() require("fzf-lua").grep_visual() end, mode = "v", desc = "visual grep" },
-    { "<leader>fgf", function() require("fzf-lua").git_files() end,       desc = "search git file names" },
-    { "<leader>fgb", function() require("fzf-lua").git_branches() end,    desc = "search git branches" },
-    { "<leader>fgc", function() require("fzf-lua").git_commits() end,     desc = "search git commits" },
-    { "<leader>fgC", function() require("fzf-lua").git_bcommits() end,    desc = "search git buffer commits" },
     { "<leader>fp",  function() require("fzf-lua").resume() end,          desc = "resume fzf" },
-    { "<leader>fu",  function() require("fzf-lua").grep_cword() end,      desc = "search word under cursor" },
-    { "<leader>fU",  function() require("fzf-lua").grep_cWORD() end,      desc = "search WORD under cursor" },
+    { "<leader>fw",  function() require("fzf-lua").grep_cword() end,      desc = "search word under cursor" },
+    { "<leader>fW",  function() require("fzf-lua").grep_cWORD() end,      desc = "search WORD under cursor" },
     { "<leader>fj",  function() require("fzf-lua").jumps() end,           desc = "search jumps" },
     { "<leader>fr",  function() require("fzf-lua").registers() end,       desc = "search registers" },
     { "<leader>fh",  function() require("fzf-lua").search_history() end,  desc = "search history" },
     { "<leader>fb",  function() require("fzf-lua").buffers() end,         desc = "search buffers" },
+    { "<leader>fd",  function() require("fzf-lua").diagnostics_document() end, desc = "search diagnostics" },
+    { "<leader>fo",  function() require("fzf-lua").oldfiles() end,        desc = "search oldfiles" },
+    { "<leader>fn",  function() require("fzf-lua").nvim_options() end,    desc = "search nvim options" },
+    { "<leader>fe",  function() require("fzf-lua").helptags() end,        desc = "search helptags" },
+
+    { "<leader>fgs", function() require("fzf-lua").git_status() end,      desc = "search git status" },
+    { "<leader>fgf", function() require("fzf-lua").git_files() end,       desc = "search git file names" },
+    { "<leader>fgb", function() require("fzf-lua").git_branches() end,    desc = "search git branches" },
+    { "<leader>fgc", function() require("fzf-lua").git_commits() end,     desc = "search git commits" },
+    { "<leader>fgC", function() require("fzf-lua").git_bcommits() end,    desc = "search git buffer commits" },
+    { "<leader>fgl", function() require("fzf-lua").git_blame() end,       desc = "search git blame" },
   },
   config = function()
-    require("fzf-lua").setup {
+    local fzf_lua = require("fzf-lua")
+    fzf_lua.setup {
+      fzf_opts = {
+        ["--cycle"]       = true,
+        ["--layout"]      = false,
+      },
+      winopts = {
+        height            = 0.90,
+        width             = 0.90,
+      },
+      defaults = {
+        formatter         = "path.dirname_first", -- show greyed-out directory before filename
+      },
+      keymap = {
+        builtin = {
+          true,
+          ["<C-c>"]       = "hide",     -- hide fzf-lua, `:FzfLua resume` to continue
+          ["<M-j>"]       = "preview-down",
+          ["<M-k>"]       = "preview-up",
+        },
+      },
       actions = {
         files = {
-          ["ctrl-l"]      = FzfLua.actions.file_vsplit,
+          true,
+          ["ctrl-l"]      = fzf_lua.actions.file_vsplit,
         },
 
       },     -- Fzf "accept" binds
-      fzf_opts = {
-        ["--layout"]         = false,
-      },    -- Fzf CLI flags
+      fzf_lua.register_ui_select()
     }
-    require('fzf-lua').register_ui_select()
   end
 }
