f9d0334f120633150380d3491b1d4ba668a1667f
Michal Witwicki <michal.witwicki14@gmail.com>
2026-04-14 16:10:02 +0200

    replace treesitter plugin
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ":TSUpdate",
-  opts = {
-    -- A list of parser names, or "all"
-      ensure_installed = {
+  'romus204/tree-sitter-manager.nvim',
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
+  config = function()
+    require("tree-sitter-manager").setup({
+      ensure_installed = { -- list of parsers to install at the start of a neovim session
         "c",
         "lua",
         "python",
@@ -22,24 +22,12 @@ return {
         "doxygen",
         "editorconfig",
       },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-    highlight = {
-      -- `false` will disable the whole extension
-        enable = true,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-  }
+      -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+      auto_install = true, -- if enabled, install missing parsers when editing a new file
+      -- highlight = true, -- treesitter highlighting is enabled by default
+      -- languages = {}, -- override or add new parser sources
+      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+    })
+  end
 }
-
